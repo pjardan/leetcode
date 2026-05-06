21a62dcc6f79e3c38fbf8603bdfe3fb57f861763
Pedro Jardim <pedro.d.jardim@hotmail.com>
2026-05-05 23:57:29 -0300

    Implements Median of Two Sorted Arrays algorithm

--- javascript/arrays/04/solution.js
+++ javascript/arrays/04/solution.js
@@ -1,25 +1,17 @@
-// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
-
-// The overall run time complexity should be O(log (m+n)).
-
- 
-
-// Example 1:
-
-// Input: nums1 = [1,3], nums2 = [2]
-// Output: 2.00000
-// Explanation: merged array = [1,2,3] and median is 2.
-// Example 2:
-
-// Input: nums1 = [1,2], nums2 = [3,4]
-// Output: 2.50000
-// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
-
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    
+    const merged = [...nums1, ...nums2].sort((a, b) => a - b);
+    const mid = Math.floor(merged.length / 2);
+    if (merged.length % 2 === 0) {
+        return (merged[mid - 1] + merged[mid]) / 2;
+    }   else {
+        return merged[mid];
+    }    
 };
+
+findMedianSortedArrays([1,3], [2]) // 2.00000
+findMedianSortedArrays([1,2], [3,4]) // 2.50000
